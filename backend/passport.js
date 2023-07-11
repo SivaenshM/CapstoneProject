d29953eadb10ed8cb6cda18179f990bfe8958def
SivaenshM <shivaaarthi08@gmail.com>
2023-07-11 19:38:07 +0400

    first commit

---
+++ backend/passport.js
@@ -0,0 +1,60 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID = "691032588327-8mntldv0u8mqn1ten4gre7ac3u1dnnqc.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "b99b0e9bab7cfd82ead2";
+GITHUB_CLIENT_SECRET = "cfd0bc94a2b8bc815f61ad3186f848e6a1028131";
+
+FACEBOOK_APP_ID = "your id";
+FACEBOOK_APP_SECRET = "your id";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
---
+++ client/src/data.js
@@ -0,0 +1,27 @@
+export const posts = [
+    {
+      id: 1,
+      title: "A Serendipitous Encounter",
+      img: "https://images.pexels.com/photos/17444442/pexels-photo-17444442/free-photo-of-picture-of-bride-and-groom.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: "Aryan and Riya met by chance on a rainy day and instantly connected. Their chance encounter blossomed into a beautiful love story, filled with laughter, adventures, and unwavering support for one another.",
+      longDesc:
+        "Once upon a time, in a bustling city, there lived two individuals named Aryan and Riya. On a rainy day, fate intervened and brought them together. Seeking shelter under the same tree, they struck up a conversation that flowed effortlessly. It was as if their souls had recognized each other, and they felt an instant connection. As the rain poured down around them, they found solace in each other's company and shared their dreams, fears, and aspirations. Little did they know that this chance encounter would mark the beginning of a profound love story. With time, Aryan and Riya's relationship grew deeper, and they embarked on a journey filled with laughter, adventures, and unwavering support for one another. They discovered that their connection was not just based on the circumstances of their meeting but on a genuine and enduring bond that transcended time and space.",
+    },
+    {
+      id: 2,
+      title: "Love Across Borders",
+      img: "https://images.pexels.com/photos/16867059/pexels-photo-16867059/free-photo-of-wedding-rings-with-couple-in-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: "Tara, an artist from India, and Ravi, a writer from the United States, defied geographical boundaries and found love. Despite the distance, they built a strong connection through shared passions and unwavering dedication.",
+      longDesc:
+        "In a world full of boundaries and limitations, Tara and Ravi proved that love knows no bounds. Tara, an artist from the vibrant land of India, and Ravi, a talented writer from the United States, found each other through an online platform. Despite the miles separating them, they discovered a profound connection through their shared passions and interests. Their conversations flowed effortlessly, and they soon realized that their hearts beat in sync, regardless of the geographical divide. Through video calls, letters, and surprise visits, they bridged the physical distance and cultivated a deep emotional bond. Their love story became a testament to the power of love, as they defied cultural and geographical barriers, proving that true love can flourish and endure, no matter the circumstances.",
+    },
+    {
+      id: 3,
+      title: "A Lifetime of Togetherness",
+      img: "https://images.pexels.com/photos/4873585/pexels-photo-4873585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: "Neha and Sameer, childhood friends turned soulmates, embraced the journey of marriage with grace and gratitude, standing by each other through life's ups and downs.",
+      longDesc:
+        "Neha and Sameer's love story started long before they realized they were destined to be together. Growing up as childhood friends in a small town, they shared countless memories, laughter, and secrets. As they transitioned from playmates to teenagers, a subtle shift occurred, and their bond deepened into something more profound. When they finally acknowledged their feelings, it was as if they had discovered a hidden treasure within their friendship. With their families' blessings, they took the plunge into matrimony, vowing to support and cherish each other for a lifetime. Their love story became an inspiration to those around them, as they faced life's challenges together, celebrating each triumph and finding solace in each other during moments of despair. Neha and Sameer's enduring love served as a reminder that true love is not merely a fleeting feeling but a lifelong commitment to stand by each other through thick and thin.",
+    },
+  ];
+  
